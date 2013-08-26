e6a188a2d98ddc7c70e40c969c24be741518d56b
Mike Boers <github@mikeboers.com>
2013-08-25 17:04:21 -0700

    Trying for service discovery from URLs, but stuck

--- Info.lua
+++ Info.lua
@@ -1,26 +1,26 @@
 return {
-	
-	LrSdkVersion = 4.0,
-	LrSdkMinimumVersion = 4.0,
+    
+    LrSdkVersion = 4.0,
+    LrSdkMinimumVersion = 4.0,
 
-	LrToolkitIdentifier = 'com.mikeboers.lightroom.publishlinker',
+    LrToolkitIdentifier = 'com.mikeboers.lightroom.publishlinker',
 
-	LrPluginName = 'Publish Linker',
-	
-	LrLibraryMenuItems = {
+    LrPluginName = 'Publish Linker',
+    
+    LrLibraryMenuItems = {
 
-		{
-			title = "Link Collection",
-			file  = "LinkCollection.lua",
-		},	
+        {
+            title = "Link Collection",
+            file  = "LinkCollection.lua",
+        },
 
-	},
+    },
 
-	VERSION = {
-		major = 0,
-		minor = 0,
-		revision = 1,
-		-- build = 1,
-	},
+    VERSION = {
+        major = 0,
+        minor = 0,
+        revision = 1,
+        -- build = 1,
+    },
 
 }
--- LinkCollection.lua
+++ LinkCollection.lua
@@ -6,9 +6,11 @@ local LrView = import 'LrView'
 local LrLogger = import 'LrLogger'
 local LrTasks = import 'LrTasks'
 
+local KnownServices = require 'KnownServices'
+
+
 local log = LrLogger()
 log:enable('print')
-
 
 
 local catalog = LrApplication.activeCatalog()
@@ -17,26 +19,32 @@ local sources = catalog:getActiveSources()
 
 --[[
 if #sources ~= 1 then
-	LrDialogs.message('Please select one published collection.', nil, 'critical')
-	return nil
+    LrDialogs.message('Please select one published collection.', nil, 'critical')
+    return nil
 end
 
 local catalog = sources[1]
 if catalog:type() ~= "LrPublishedCollection" then
-	LrDialogs.message("Please select a published collection.", nil, 'critical')
-	return nil
+    LrDialogs.message("Please select a published collection.", nil, 'critical')
+    return nil
 end
 ]]
 
 LrFunctionContext.postAsyncTaskWithContext('PublishLinker_LinkCollection', function(context)
 
-	local services = catalog:getPublishServices()
-	local flickrService
-	for _, service in ipairs(services) do
-		if service:getPluginId() == 'com.adobe.lightroom.export.flickr' then
-			flickrService = service
-		end
-	end
+    local services = catalog:getPublishServices()
+    local collectionItems = {}
+    local append = function(t, x) t[#t + 1] = x end
+    for _, service in ipairs(services) do
+
+        local serviceDef = KnownServices.serviceById(service:getPluginId())
+        if serviceDef then
+            append(collectionItems, {
+                title = serviceDef.name .. ': (new)',
+                value = #collectionItems, 
+            })
+        end
+    end
 
     local ui = LrView.osFactory()
 
@@ -46,19 +54,37 @@ LrFunctionContext.postAsyncTaskWithContext('PublishLinker_LinkCollection', funct
     -- Create the contents for the dialog.
     local c = ui:column {
 
-	    bind_to_object = props,
-		spacing = ui:dialog_spacing(),
+        bind_to_object = props,
+        spacing = ui:control_spacing(),
 
-	    ui:edit_field {
-		    value = LrView.bind('remoteUrl'),
-		    width = 600,
-	    },
+        ui:row {
+            ui:static_text {
+                title = 'Collection: ',
+                alignment = 'right',
+                width = LrView.share 'label_width', 
+            },
+            ui:popup_menu {
+                items = collectionItems,
+            },
+        },
+
+        ui:row {
+            ui:static_text {
+                title = 'Remote URL: ',
+                alignment = 'right',
+                width = LrView.share 'label_width',
+            },
+            ui:edit_field {
+                value = LrView.bind 'remoteUrl',
+                width = 600,
+            },
+        },
 
     }
 
     local res = LrDialogs.presentModalDialog {
-	    title = "Link Published Collection",
-	    contents = c,
+        title = "Link Published Collection",
+        contents = c,
     }
     if res ~= 'ok' then return nil end
 
@@ -67,13 +93,13 @@ LrFunctionContext.postAsyncTaskWithContext('PublishLinker_LinkCollection', funct
     log:info('remote ID', remoteId)
 
     LrTasks.startAsyncTask(function()
-    	catalog:withWriteAccessDo('Create Published Collection', function()
-	    	log:info('creating collection...')
-		    local collection = flickrService:createPublishedCollection(remoteId, nil, true)
-		    log:info('created collection', collection)
-		    collection:setRemoteId(remoteId)
-		    collection:setRemoteUrl(props.remoteUrl)
-		end)
+        catalog:withWriteAccessDo('Create Published Collection', function()
+            log:info('creating collection...')
+            local collection = flickrService:createPublishedCollection(remoteId, nil, true)
+            log:info('created collection', collection)
+            collection:setRemoteId(remoteId)
+            collection:setRemoteUrl(props.remoteUrl)
+        end)
     end)
 
 end)
